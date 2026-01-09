a42dbb6469846aea0e8706e1a9e949587ed1ddff
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2026-01-09 09:53:23 +0200

    Custom instance

---
+++ 05-axios-tutorial/src/axios/global.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+//The global defaults are going to be applied to every request we make
+
+axios.defaults.headers.common['Accept'] = 'application/json';
--- 05-axios-tutorial/src/examples/4-global-instance.js
+++ 05-axios-tutorial/src/examples/4-global-instance.js
@@ -1,11 +1,18 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const productsUrl = 'https://www.course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try
+    {
+        const res1 = await axios(productsUrl);
+        const res2 = await axios(randomUserUrl);
+        console.log(res1);
+    } catch(error){};
+    
   };
 
   useEffect(() => {

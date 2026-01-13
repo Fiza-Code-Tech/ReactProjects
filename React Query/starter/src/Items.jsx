c9eedecf42417b04da71e9e226bf067dddfcc815
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2026-01-13 11:10:42 +0200

    The useQuery & Error Handling

--- React Query/starter/src/Items.jsx
+++ React Query/starter/src/Items.jsx
@@ -1,8 +1,34 @@
 import SingleItem from './SingleItem';
+import instance from './utils';
+import { useQuery } from '@tanstack/react-query';
 const Items = ({ items }) => {
+
+  const {data, isError, isLoading, error} = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const { data } = await instance.get('/');
+      return data;
+    },
+  });
+
+  console.log(data);
+  if(isLoading){
+    return <p style={{marginTop: '1rem'}}>Loading...</p>
+  }
+
+  /*
+  if(isError)
+  {
+    return <p style={{marginTop: '1rem'}}>There was an error</p>;
+  }*/
+  
+  if(error)
+  {
+    return <p style={{marginTop: '1rem'}}>{error.message}</p>
+  }
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
